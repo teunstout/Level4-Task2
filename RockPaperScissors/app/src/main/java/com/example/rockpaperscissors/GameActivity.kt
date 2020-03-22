197e0570ddb09df8e065c698e4147d7440518280
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-03-22 12:46:00 +0100

    Room in project and view done

--- RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/GameActivity.kt
+++ RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/GameActivity.kt
@@ -1,12 +1,37 @@
 package com.example.rockpaperscissors
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.game_activity.*
 
 class GameActivity : AppCompatActivity() {
+    private val ROCK = "ROCK"
+    private val PAPER = "PAPER"
+    private val SCISSORS = "SCISSORS"
+    private val gamePictures = arrayListOf(R.drawable.rock, R.drawable.paper, R.drawable.scissors)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_activity)
+
+        imgButtonRock.setOnClickListener { checkAwnser(ROCK) }
+        imgButtonPaper.setOnClickListener { checkAwnser(PAPER) }
+        imgButtonScissors.setOnClickListener { checkAwnser(SCISSORS) }
+
+        initView()
+    }
+
+    private fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun checkAwnser(awnser: String){
+        when(awnser){
+            ROCK -> {}
+            PAPER -> {}
+            SCISSORS -> {}
+        }
     }
 }
---
+++ RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/ui/PlayedGame.kt
@@ -0,0 +1,7 @@
+package com.example.rockpaperscissors.ui
+
+
+data class PlayedGame(
+
+
+)
